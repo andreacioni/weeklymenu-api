c40a8b6723bd7cfc54f09a4da4fa8d342ef9e5b3
Andrea Cioni <supercionci@hotmail.it>
2019-09-19 22:47:57 +0200

    setup pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,21 @@ import pytest
 from flask.testing import FlaskClient
 from weekly_menu import create_app
 
+from weekly_menu.webapp.api.models import Ingredient, Menu, Recipe, User, ShoppingList
+
+@pytest.fixture(autouse=True)
+def clear_db():
+  yield
+  #Clear all collections
+  User.drop_collection()
+  Recipe.drop_collection()
+  Menu.drop_collection()
+  Ingredient.drop_collection()
+  ShoppingList.drop_collection()
+
 @pytest.fixture(scope='session')
 def client():
     app = create_app('pytest-config')
     testing_client = app.test_client()
-    ctx = app.app_context()
-    ctx.push()
  
-    yield testing_client
- 
-    ctx.pop()
+    return testing_client
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,3 +12,12 @@ def test_bad_request_registration(client: FlaskClient):
   response = client.post('/api/v1/auth/register', json={'username':'a'})
 
   assert response.status_code == 400
+
+def test_user_creation(client: FlaskClient):
+  response = client.post('/api/v1/auth/register', json={
+    'username':'test', 
+    'password':'pippofranco',
+    'email':'pippo@franco.com'
+    })
+
+  assert response.status_code == 200
