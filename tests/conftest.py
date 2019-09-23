e4ebcb4a1df100d63b74305fb5f71a87ce63aa2a
Andrea Cioni <supercionci@hotmail.it>
2019-09-23 23:00:02 +0200

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
+import jwt
 
 from flask.testing import FlaskClient
 from weekly_menu import create_app
 
 from weekly_menu.webapp.api.models import Ingredient, Menu, Recipe, User, ShoppingList
+from weekly_menu.webapp.api.v1.auth import encode_password
+
+TEST_USERNAME = 'test'
+TEST_PASSWORD = 'a@b.it'
+TEST_EMAIL = 'pippofranco'
 
 @pytest.fixture(autouse=True)
 def clear_db():
@@ -16,8 +22,24 @@ def clear_db():
   ShoppingList.drop_collection()
 
 @pytest.fixture(scope='session')
-def client():
-    app = create_app('pytest-config')
-    testing_client = app.test_client()
- 
-    return testing_client
+def app():
+  return create_app('pytest-config')
+
+@pytest.fixture(scope='session')
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def auth_headers(app):
+  user = User()
+  user.username = TEST_USERNAME
+  user.email = TEST_EMAIL
+  user.password = encode_password(TEST_EMAIL)
+  user.save()
+
+  valid_token = jwt.encode({'username':'username'}, app.config['SECRET_KEY']).decode('utf-8')
+  headers = {
+      'Authorization': 'Bearer {}'.format(valid_token)
+  }
+
+  return headers
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,6 +4,8 @@ from flask import jsonify
 from flask.json import dumps, loads
 from flask.testing import FlaskClient
 
+from conftest import TEST_USERNAME, TEST_PASSWORD, TEST_EMAIL
+
 def test_bad_request_registration(client: FlaskClient):
   response = client.post('/api/v1/auth/register', json={})
 
@@ -15,9 +17,23 @@ def test_bad_request_registration(client: FlaskClient):
 
 def test_user_creation(client: FlaskClient):
   response = client.post('/api/v1/auth/register', json={
-    'username':'test', 
-    'password':'pippofranco',
-    'email':'pippo@franco.com'
+    'username':TEST_USERNAME, 
+    'password':TEST_PASSWORD,
+    'email':TEST_EMAIL
     })
 
   assert response.status_code == 200
+
+  response = client.post('/api/v1/auth/token', json={
+    'username':TEST_USERNAME, 
+    'password':'wrong-password'
+  })
+
+  assert response.status_code == 401
+
+  response = client.post('/api/v1/auth/token', json={
+    'username':TEST_USERNAME, 
+    'password':TEST_PASSWORD
+  })
+
+  assert response.status_code == 200 and response.json['access_token'] is not None
