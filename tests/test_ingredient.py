d16114f6c510013eb26da6e82bb4c0de343223d8
Andrea Cioni <supercionci@hotmail.it>
2019-09-28 00:25:39 +0200

    more tests

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -7,6 +7,9 @@ from flask.testing import FlaskClient
 def create_ingredient(client, json, auth_headers):
   return client.post('/api/v1/ingredients', json=json, headers=auth_headers)
 
+def update_ingredient(client, ing_id, json, auth_headers):
+  return client.patch('/api/v1/ingredients/{}'.format(ing_id), json=json, headers=auth_headers)
+
 def get_all_ingredients(client, auth_headers, page=1, per_page=10):
   return client.get('/api/v1/ingredients?page={}&per_page={}'.format(page, per_page), headers=auth_headers)
 
@@ -45,8 +48,21 @@ def test_create_ingredient(client: FlaskClient, auth_headers):
 
   assert response.status_code == 200 and response.json['pages'] == 2
 
+def test_update_ingredient(client: FlaskClient, auth_headers):
+  response = create_ingredient(client, {
+    'name': 'Tuna',
+    'description': 'this is a tuna'
+  } , auth_headers)
 
+  assert response.status_code == 201 and response.json['name'] == 'Tuna' and response.json['description'] == 'this is a tuna'
 
+  response = update_ingredient(client, response.json['_id']['$oid'], {
+    'name': 'Tuna',
+    'description': 'always a tuna',
+    'note': 'note about tuna'
+  }, auth_headers)
+  
+  assert response.status_code == 200 and response.json['description'] == 3
 
 
 
---
+++ tests/test_recipe.py
@@ -0,0 +1,86 @@
+import pytest
+
+from flask import jsonify
+from flask.json import dumps, loads
+from flask.testing import FlaskClient
+
+from test_ingredient import create_ingredient
+
+def create_recipe(client, json, auth_headers):
+  return client.post('/api/v1/recipes', json=json, headers=auth_headers)
+
+def update_recipe(client, recipe_id, json, auth_headers):
+  return client.patch('/api/v1/recipes/{}'.format(recipe_id), json=json, headers=auth_headers)
+
+def get_all_recipes(client, auth_headers, page=1, per_page=10):
+  return client.get('/api/v1/recipes?page={}&per_page={}'.format(page, per_page), headers=auth_headers)
+
+def test_not_authorized(client: FlaskClient):
+  response = get_all_recipes(client, {})
+  
+  assert response.status_code == 401
+
+def test_create_recipe(client: FlaskClient, auth_headers):
+  response = get_all_recipes(client, auth_headers)
+
+  assert response.status_code == 200 and len(response.json['results']) == 0 and response.json['pages'] == 0
+
+  tuna_resp = create_ingredient(client, {
+    'name' : 'Tuna'
+  }, auth_headers)
+
+  tomato_resp = create_ingredient(client, {
+    'name' : 'Tomatoes'
+  }, auth_headers)
+
+  response = create_recipe(client, {
+    'name': 'Tuna and tomatoes',
+    'ingredients' : [
+      {
+      'ingredient': tuna_resp.json['_id']['$oid']
+      },{
+      'ingredient': tomato_resp.json['_id']['$oid']
+      }
+    ]
+  } , auth_headers)
+
+  assert response.status_code == 201 and response.json['name'] == 'Tuna and tomatoes'
+
+  # Test fail duplicating ingredient
+  response = create_recipe(client, {
+    'name': 'Tuna and tomatoes'
+  } , auth_headers)
+
+  assert response.status_code == 409
+
+  response = create_recipe(client, {
+    'name': 'Pizza'
+  } , auth_headers)
+
+  assert response.status_code == 201 and response.json['name'] == 'Pizza'
+
+  #Check pagination 
+  response = get_all_recipes(client, auth_headers, 1, 1)
+
+  assert response.status_code == 200 and response.json['pages'] == 2
+
+def test_update_recipe(client: FlaskClient, auth_headers):
+  response = create_recipe(client, {
+    'name': 'Tuna and tomatoes',
+    'servs': 2
+  } , auth_headers)
+
+  assert response.status_code == 201 and response.json['servs'] == 2
+
+  response = update_recipe(client, response.json['_id']['$oid'], {
+    'name': 'Tuna and tomatoes',
+    'servs': 3
+  }, auth_headers)
+  
+  assert response.status_code == 200 and response.json['servs'] == 3
+
+
+
+
+
+
