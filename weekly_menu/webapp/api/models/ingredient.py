f74fb444a1a9f9d9a4a85532389360b4d30710ca
Andrea Cioni <supercionci@hotmail.it>
2020-02-17 23:04:57 +0100

    changed ingredient schema

--- weekly_menu/webapp/api/models/ingredient.py
+++ weekly_menu/webapp/api/models/ingredient.py
@@ -4,13 +4,13 @@ class Ingredient(mongo.Document):
     name = mongo.StringField(required=True)
     description = mongo.StringField()
     note = mongo.StringField()
-    edible = mongo.BooleanField(required=True, default=True)
-    freezed = mongo.BooleanField(default=False)
+    edible = mongo.BooleanField()
+    freezed = mongo.BooleanField()
     availabilityMonths = mongo.ListField(
-        mongo.IntField(min_value=1, max_value=12), max_length=12
+        mongo.IntField(min_value=1, max_value=12), max_length=12, default=None
     )
     tags = mongo.ListField(
-        mongo.StringField()
+        mongo.StringField(), default=None
     )
     
     owner = mongo.ReferenceField('User', required=True)
