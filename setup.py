a327d3029a8a0ee0e14d2111fba555ff2a2bdf97
Andrea Cioni <acioni@florence-consulting.it>
2020-08-05 22:44:35 +0200

    Releasing v0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weekly-menu",
-    version="0.0.9",
+    version="0.0.10",
     author="Andrea Cioni",
     description="Automate your weekly menu with AI",
     long_description=long_description,
