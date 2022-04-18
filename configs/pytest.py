725ca4fe8fef185efd1bb50286d255004f23e33e
Andrea Cioni <supercionci@hotmail.it>
2022-04-18 12:48:20 +0200

    add cors support

--- configs/pytest.py
+++ configs/pytest.py
@@ -7,6 +7,9 @@ API_PORT=8080
 JSON_SORT_KEYS=False
 JWT_ACCESS_TOKEN_EXPIRES=60
 
+#CORS
+CORS_ORIGINS=['http://localhost:8080/']
+
 #MongoDb
 MONGODB_SETTINGS={
     'host': 'mongomock://localhost:27017/test?socketTimeoutMS=1000'
