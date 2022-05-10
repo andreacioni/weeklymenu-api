a92b73331e1a82cda678ec55ca47429708a6db83
Andrea Cioni <supercionci@hotmail.it>
2022-05-10 23:51:18 +0200

    renamed class

--- weekly_menu/webapp/api/models/user_preferences.py
+++ weekly_menu/webapp/api/models/user_preferences.py
@@ -1,14 +1,14 @@
 from .. import mongo
 from .base_document import BaseDocument
 
-class SupermarketSectionPreference(mongo.EmbeddedDocument):
+class SupermarketSection(mongo.EmbeddedDocument):
     MAX_SECTION_NAME_LENGTH = 50
 
     name = mongo.StringField(max_length=MAX_SECTION_NAME_LENGTH)
     color = mongo.StringField(max_length=MAX_SECTION_NAME_LENGTH)
 
 class UserPreferences(BaseDocument):
-    supermarket_sections = mongo.EmbeddedDocumentListField(SupermarketSectionPreference, default=None)
+    supermarket_sections = mongo.EmbeddedDocumentListField(SupermarketSection, default=None)
     
     shopping_days = mongo.ListField(mongo.IntField(min_value=1, max_value=7), default=None)
 
