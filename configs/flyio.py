301474c887cc278f163a8f9f4f1e29de75f5dad6
Andrea Cioni <supercionci@hotmail.it>
2023-01-29 11:26:54 +0100

    spacy model to parse ingredients

--- configs/flyio.py
+++ configs/flyio.py
@@ -1,21 +1,24 @@
 import os
 
-JSON_SORT_KEYS=os.getenv('JSON_SORT_KEYS',False)
+JSON_SORT_KEYS = os.getenv('JSON_SORT_KEYS', False)
 
-#CORS
-CORS_ORIGINS=['https://weeklymenu.altervista.org']
+# CORS
+CORS_ORIGINS = ['https://weeklymenu.altervista.org']
 
-#MongoDb
-MONGODB_SETTINGS={
+# MongoDb
+MONGODB_SETTINGS = {
     'host': os.getenv('MONGO_HOST', None)
 }
 
-#JWT
-SECRET_KEY=os.getenv('SECRET_KEY','NONE')
-JWT_ACCESS_TOKEN_EXPIRES=os.getenv('JWT_ACCESS_TOKEN_EXPIRES',15778800)
+# JWT
+SECRET_KEY = os.getenv('SECRET_KEY', 'NONE')
+JWT_ACCESS_TOKEN_EXPIRES = os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 15778800)
 
-#Log
-LOG_LEVEL=os.getenv('LOG_LEVEL', None)
-LOG_FILE=os.getenv('LOG_FILE', None)
+# Log
+LOG_LEVEL = os.getenv('LOG_LEVEL', None)
+LOG_FILE = os.getenv('LOG_FILE', None)
 LOG_MAX_SIZE = os.getenv('LOG_MAX_SIZE', 10000000)
 LOG_BACKUP_COUNT = os.getenv('LOG_BACKUP_COUNT', 3)
+
+# Models
+MODELS_BASE_PATH = os.getenv('MODELS_BASE_PATH', '/etc/weeklymenu/models')
