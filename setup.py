f64a3e3efa1d0206f638c4879f7c27d979c13f93
Andrea Cioni <supercionci@hotmail.it>
2024-01-07 16:25:51 +0100

    Releasing v0.0.53

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weekly-menu",
-    version="0.0.52",
+    version="0.0.53",
     author="Andrea Cioni",
     description="Automate your weekly menu with AI",
     long_description=long_description,
